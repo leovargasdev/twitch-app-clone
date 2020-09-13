f10d7ffe2b25e321d90cf3b626ea30cd67aed1ed
LeeonardoVargas <leu1607@hotmail.com>
2020-09-13 11:37:00 -0300

    Adicionando estilo e os icones na tabBar

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,60 @@
 import React from 'react';
-import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import Following from './pages/Following';
 import ComingSoon from './pages/ComingSoon';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
-const Routes: React.FC = () => (
-  <NavigationContainer>
-    <Navigator>
-      <Screen name="Following" component={Following} />
-      <Screen name="Discover" component={ComingSoon} />
-      <Screen name="Browse" component={ComingSoon} />
-      <Screen name="Esports" component={ComingSoon} />
-    </Navigator>
-  </NavigationContainer>
+const IconNav = ({name, ...props}: {name: string}) => (
+  <MaterialCommunityIcons name={name} {...props}/>
 );
+
+const Routes: React.FC = () => {
+  const {colors, fonts} = useTheme();
+  return (
+    <NavigationContainer>
+      <Navigator tabBarOptions={{
+        style: {
+          backgroundColor: colors.primary,
+          borderTopWidth: 0,
+        },
+        tabStyle: {
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        iconStyle: {
+          width: 20,
+          height: 20,
+        },
+        labelStyle: {
+          fontFamily: fonts.regular,
+          fontSize: 11,
+        },
+        inactiveTintColor: colors.secondary,
+        activeTintColor: colors.purple
+      }}>
+        <Screen name="Following" component={Following} options={{
+          tabBarIcon: ({...props}) => <IconNav name="heart" {...props}/>
+        }}
+        />
+        <Screen name="Discover" component={ComingSoon} options={{
+          tabBarIcon: ({...props}) => <IconNav name="compass-outline" {...props}/>
+        }}
+        />
+        <Screen name="Browse" component={ComingSoon} options={{
+          tabBarIcon: ({...props}) => <IconNav name="magnify" {...props}/>
+        }}
+        />
+        <Screen name="Esports" component={ComingSoon} options={{
+          tabBarIcon: ({...props}) => <IconNav name="trophy-outline" {...props}/>
+        }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Routes;
